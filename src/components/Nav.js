7e3da5047898ffa979a8cf5696f93f79c1dc471a
Simphiwe <madsim321@gmail.com>
2025-06-20 13:23:35 +0200

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import './App.css';
+
+import React, { useState } from "react";
+import logo from "../images/logo.jpg"
 
 const Nav = () => {
+  const [menuOpen,setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+  
   return (
-    <nav>
-      <a href = '/'></a>
+    <nav className={'navbar'}>
+     <a href="/" className='logo'>
+      <img src={logo} alt="logo"/>
+     </a>
+
+     <div>
+      <div className='bar'></div>
+      <div className='bar'></div>
+      <div className='bar'></div>
+     </div>
+
+     <ul classname={'nav-links'}>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/">About</a>
+      </li>
+      <li>
+        <a href="/">Services</a>
+      </li>
+      <li>
+        <a href="/">Menu</a>
+      </li>
+      <li>
+        <a href="/">Bookings</a>
+      </li>
+      <li>
+        <a href="/">Order Online</a>
+      </li>
+      <li>
+        <a href="/">Login</a>
+      </li>
+     </ul>
     </nav>
   );
 }
